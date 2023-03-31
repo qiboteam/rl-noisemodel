6b2e39eabac466edd9239c76a3e81d5bfd241de5
simone bordoni <simone.bordoni@uniroma1.it>
2023-03-31 12:57:14 +0400

    padding

---
+++ src/rlnoise/envs/gym_env_v2.py
@@ -0,0 +1,102 @@
+import numpy as np
+import gym, random
+from gym import spaces
+from qibo import gates
+from qibo.models import Circuit
+from copy import deepcopy
+from rlnoise.rewards.density_matrix_reward import dm_reward_stablebaselines, step_reward_stablebaselines
+
+# currently working just for single qubit circuits
+# TO DO:
+# - Adapt to multi-qubits circuits
+# - Implement batches
+# - Write a reward that makes sense
+# - Adapt it for working with the 3d representation
+
+
+class QuantumCircuit(gym.Env):
+    
+    def __init__(self, circuits, noise_channel, representation, labels, reward, window=5):
+        super(QuantumCircuit, self).__init__()
+
+        self.circuits = circuits[:,np.newaxis,:,:]
+        self.n_circ = self.circuits.shape[0]
+        self.n_moments = self.circuits.shape[2]
+        self.noise_channel = noise_channel
+        self.rep = representation
+        self.n_gate_types = len(self.rep.gate2index)
+        self.n_channel_types = len(self.rep.channel2index)
+        self.labels = labels
+        self.reward = reward
+        self.window = window
+        assert self.window%2==1, 'Window must be odd'
+        shape = list(self.circuits.shape[1:])
+        assert shape[-1] % (self.rep.encoding_dim) == 0
+        self.n_qubits = int(shape[-1] / self.rep.encoding_dim)
+        self.circuit_shape=shape
+
+        assert self.n_channel_types == 1, "Multiple possible channels not implemented yet"
+        
+        self.observation_space = spaces.Box(
+            low = 0,
+            high = 1,
+            shape = tuple(shape),
+            dtype = np.float32
+        )
+        self.action_space = spaces.MultiBinary(self.n_qubits * self.n_channel_types)
+        self.current_state = None
+    
+    def _get_obs(self):
+        return self.circuit_padding[:,self.pos:self.pos+self.window,:]
+
+    def _get_info(self):
+        return { 'state': self._get_obs() } 
+
+    def reset(self, i=None):
+        if i is None:
+            i = random.randint(0, self.n_circ - 1)
+        self.sample = i
+        self.pos = 0
+        state = self.circuits[i].squeeze(0)
+        state = state[np.newaxis,:,:]
+        padding = np.zeros((1, int((self.window-1)/2), self.circuit_shape[-1]), dtype=np.float32)
+        self.circuit_padding = np.concatenate((padding,state,padding), axis=1)
+        return self._get_obs()
+
+    def step(self, action):
+        #print('> State:\n', self._get_obs())
+        action = action.reshape(self.n_qubits, -1) #action.shape=(num_qubits, 1)        
+        #print(f'> Position: {position}, Action: {action} , Action shape: {action.shape}')
+        for q, a in enumerate(action):
+            if a == 1:
+                idx = q * self.rep.encoding_dim
+                self.current_state[0, self.pos, idx:idx+self.rep.encoding_dim] += self.rep.gate_to_array(self.noise_channel)
+        #print(f'> New State: \n{self._get_obs()}')
+        terminated = False
+        if self.pos == self.n_moments - 1:
+            # compute final reward
+            terminated = True
+        self.pos+=1
+        return self._get_obs(), self.reward(self.get_qibo_circuit(), self.current_target, terminated), terminated, self._get_info()
+
+    def render(self):
+        print(self.get_qibo_circuit().draw(), end='\r')
+            
+    def get_position(self):
+        return (self.current_state[:,:,-1] == 1).nonzero()[-1]
+
+    def get_qibo_circuit(self):
+        return self.rep.array_to_circuit(self.current_state[0][:,:-1])
+
+    def compute_step_reward(self):
+        alpha=0.01
+        circuit = self.get_qibo_circuit() #circuit.shape=(n_gates,5)
+        print('Il circuito ricostruito e`: ',circuit.draw())
+        #print('Circuit.shape= ',self.rep.circuit_to_array(circuit).shape) 
+        true_circuit_label=self.labels
+        learned_labels=np.asarray(circuit().state())
+        mse = alpha*np.sqrt(np.abs(((true_circuit_label-learned_labels)**2).mean()))
+        return -mse
+            #... da continuare
+
+    
---
+++ src/rlnoise/tests/test_gym_v2.py
@@ -0,0 +1,140 @@
+import sys
+from rlnoise.rewards.rewards import FrequencyReward,DensityMatrixReward
+from rlnoise.dataset import Dataset, CircuitRepresentation
+from rlnoise.policy import CNNFeaturesExtractor
+import numpy as np
+from rlnoise.envs.gym_env_v2 import QuantumCircuit
+from stable_baselines3 import PPO, DQN, DDPG
+from qibo.noise import DepolarizingError, NoiseModel
+from qibo import gates
+from rlnoise.rewards.density_matrix_reward import dm_reward_stablebaselines
+
+nqubits = 1
+depth = 5
+ncirc = 1
+
+noise_model = NoiseModel()
+lam = 0.1
+noise_model.add(DepolarizingError(lam), gates.RZ)
+noise_channel = gates.DepolarizingChannel((0,), lam=lam)
+primitive_gates = ['RZ', 'RX']
+channels = ['DepolarizingChannel']
+
+rep = CircuitRepresentation(
+    primitive_gates = primitive_gates,
+    noise_channels = channels,
+    shape = '2d'
+)
+
+# create dataset
+dataset = Dataset(
+    n_circuits = ncirc,
+    n_gates = depth,
+    n_qubits = nqubits,
+    representation = rep,
+    clifford = True,
+    noise_model = noise_model,
+    mode = 'rep'
+)
+
+test_sample = np.random.randint(ncirc)
+
+# input circuit
+circuit_rep = dataset[test_sample]
+dataset.set_mode('circ')
+circuit = dataset[test_sample]
+dataset.set_mode('noisy_circ')
+noisy_circuit = dataset[test_sample]
+#labels = list(dataset.get_frequencies())
+labels=np.array(dataset.get_dm_labels())
+
+def test_representation():
+    print('> Noiseless Circuit:\n', circuit.draw())
+    array = rep.circuit_to_array(circuit)
+    print(' --> Representation:\n', array)
+    print(' --> Circuit Rebuilt:\n', rep.array_to_circuit(array).draw())
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('> Noisy Circuit:\n', noisy_circuit.draw())
+    array = rep.circuit_to_array(noisy_circuit)
+    print(array)
+    print(' --> Circuit Rebuilt:\n', rep.array_to_circuit(array).draw())
+
+dataset.set_mode('rep')
+reward = DensityMatrixReward()
+
+circuits=dataset[:]
+circuit_env = QuantumCircuit(
+    circuits = dataset[0][np.newaxis,:,:],
+    noise_channel = noise_channel,
+    representation = rep,
+    labels = labels,
+    reward = reward,
+)
+
+print(circuit_env.reset())
+
+"""
+policy = "MlpPolicy"
+policy_kwargs = dict(
+    features_extractor_class = CNNFeaturesExtractor,
+    features_extractor_kwargs = dict(
+        features_dim = 64,
+        filter_shape = (2, nqubits * rep.encoding_dim )
+    )
+)
+
+model = PPO(
+    policy,
+    circuit_env,
+    policy_kwargs=policy_kwargs,
+    verbose=1,
+)
+
+test_sample=0
+avg_untrained_rew =0 
+
+# Untrained Agent
+for i in range(ncirc):
+    obs = circuit_env.reset(i=i)
+    done = False
+    while not done:
+        action, _states = model.predict(obs, deterministic=True)
+        obs, rewards, done, info = circuit_env.step(action)
+    untrained_circ = rep.array_to_circuit(obs[:,:,:-1][0])
+    dm_untrained=untrained_circ().state()
+    label_dm = labels[i]
+    avg_untrained_rew += dm_reward_stablebaselines(noisy_circuit,label_dm)
+
+# Train
+model.learn(20000, progress_bar=True) #probably to put inside the for loop
+
+avg_trained_rew=0.
+# Trained Agent
+for i in range(ncirc):
+    obs = circuit_env.reset(i=i)
+    #print('Circuit %d representation \n'%(i),rep.array_to_circuit(obs[:,:,:-1][0]).draw()) #test to check that works for more circuits
+    done = False
+    while not done:
+        action, _states = model.predict(obs, deterministic=True)
+        obs, rewards, done, info = circuit_env.step(action)
+    trained_circ = rep.array_to_circuit(obs[:,:,:-1][0])
+    dm_trained=trained_circ().state()
+    label_dm = labels[i]
+    avg_trained_rew += dm_reward_stablebaselines(trained_circ,label_dm)
+    if i==test_sample:
+        test_dm=dm_trained
+        test_circ=trained_circ.copy()
+    
+
+
+print('---- Original Circuit ----\n', circuit.draw(), '\n', circuit_rep)
+print(' --> With noise\n', noisy_circuit.draw())
+print(labels[test_sample])
+
+print('---- Avg rew Before Training ----\n')
+print(avg_untrained_rew/ncirc)
+
+print('---- After Training ----\n', trained_circ.draw())
+print(dm_trained)
+print(dm_reward_stablebaselines(trained_circ,label_dm))
+"""
