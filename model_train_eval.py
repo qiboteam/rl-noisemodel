b984e5a96eb71d2cd27bed0ea006935107e63c53
pg176 <buttarini.1761445@studenti.uniroma1.it>
2023-05-09 23:23:43 +0200

    added continue action space

--- model_train_eval.py
+++ model_train_eval.py
@@ -19,13 +19,13 @@ benchmark_circ_path=os.getcwd()+'/src/rlnoise/bench_dataset'
 model_path=os.getcwd()+'/src/rlnoise/saved_models/'
 bench_results_path=os.getcwd()+'/src/rlnoise/bench_results'
 
-f = open(benchmark_circ_path+"/depth_%dDep-Term_CZ_3Q_100.npz"%(circuits_depth),"rb")
+f = open(benchmark_circ_path+"/depth_%dDep-Term_CZ_3Q_1000.npz"%(circuits_depth),"rb")
 tmp=np.load(f,allow_pickle=True)
 train_set=copy.deepcopy(tmp['train_set'])
 train_label=copy.deepcopy(tmp['train_label'])
 val_set=copy.deepcopy(tmp['val_set'])
 val_label=copy.deepcopy(tmp['val_label'])
-f.close()
+
 
 #Setting up training env and policy model
 nqubits=3
@@ -45,7 +45,8 @@ circuit_env_training = QuantumCircuit(
     representation = rep,
     labels = train_label,
     reward = reward,
-    kernel_size=kernel_size
+    kernel_size=kernel_size,
+    step_reward=False
 )
 policy = "MlpPolicy"
 policy_kwargs = dict(
@@ -61,8 +62,8 @@ Rew_Mae_TraceD_trained=[]
 #model=PPO.load(model_path+"rew_each_step_D7_box")
 
                                                 #SINGLE TRAIN AND VALID
-'''
-callback=CustomCallback(check_freq=2000,evaluation_set=tmp,train_environment=circuit_env_training,trainset_depth=circuits_depth)                                          
+
+callback=CustomCallback(check_freq=5000,evaluation_set=tmp,train_environment=circuit_env_training,trainset_depth=circuits_depth,save_best=True)                                          
 model = PPO(
 policy,
 circuit_env_training,
@@ -72,7 +73,7 @@ verbose=0,
 
 
 #val_avg_rew_untrained,mae_untrained,trace_dist_untr=(model_evaluation(val_set,val_label,circuit_env_training,model))
-model.learn(300000,progress_bar=True,callback=callback)
+model.learn(500000,progress_bar=True,callback=callback)
 #val_avg_rew_trained,mae_trained,trace_dist_train=(model_evaluation(val_set,val_label,circuit_env_training,model))
 #print('avg reward from untrained model: %f\n'%(val_avg_rew_untrained),'avg reward from trained model: %f \n'%(val_avg_rew_trained))
 #print('avg MAE from untrained model: %f\n'%(mae_untrained*10),'avg MAE from trained model: %f \n'%(mae_trained*10))
@@ -80,7 +81,7 @@ model.learn(300000,progress_bar=True,callback=callback)
 #if trace_dist_train ==0:
 #    model.save(model_path+"D5_K3_1Q_Dep_Therm_30k")
 #model.save(model_path+"D7_K3_3Q_Dep0.005_Therm0.07_80k")
-'''
+f.close()
                                         #TRAIN & TEST ON SAME DEPTH BUT DIFFERENT TIMESTEPS
 '''
 total_timesteps=[2000,4000,6000,8000,10000,15000,20000,30000,50000,80000,130000,200000]
@@ -147,34 +148,34 @@ f.close()
 
 
                         #TRAIN & TEST ON DATASET W SAME PARAMS BUT DIFFERENT SIZE(n_circ)
-n_circ=[100,1000]
-
+'''
+circuits_depth=7                       
+n_circ=[10,50,200,400]
+f = open(benchmark_circ_path+"/depth_%dDep-Term_CZ_3Q_1000.npz"%(circuits_depth),"rb")
+tmp=np.load(f,allow_pickle=True)
+val_set=copy.deepcopy(tmp['val_set'])
+val_label=copy.deepcopy(tmp['val_label'])
+train_set=copy.deepcopy(tmp['train_set'])
+train_label=copy.deepcopy(tmp['train_label'])
 for data_size in n_circ:
 
-    f = open(benchmark_circ_path+"/depth_%dDep-Term_CZ_3Q_%d.npz"%(circuits_depth,data_size),"rb")
-    tmp=np.load(f,allow_pickle=True)
-    train_set=tmp['train_set']
-    train_label=tmp['train_label']
-    val_set=tmp['val_set']
-    val_label=tmp['val_label']
-
     circuit_env_training = QuantumCircuit(
-    circuits = train_set,
+    circuits = train_set[:data_size],
     representation = rep,
-    labels = train_label,
+    labels = train_label[:data_size],
     reward = reward,
     kernel_size=kernel_size
     )
-    callback=CustomCallback(check_freq=2000,evaluation_set=tmp,train_environment=circuit_env_training,trainset_depth=circuits_depth)                                          
+    callback=CustomCallback(check_freq=2000,evaluation_set=tmp,train_environment=circuit_env_training,trainset_depth=circuits_depth,test_on_data_size=data_size)                                          
     model = PPO(
     policy,
     circuit_env_training,
     policy_kwargs=policy_kwargs, 
     verbose=0,
     )
-    a,b,c= model.learn(2000,callback=callback)
-    print(a,b,c)
-
+    model.learn(100000,progress_bar=True, callback=callback)
+f.close()
+'''
 #f.close()
 
 
--- src/rlnoise/policy.py
+++ src/rlnoise/policy.py
@@ -63,7 +63,7 @@ class CustomCallback(BaseCallback):
 
     :param verbose: (int) Verbosity level 0: not output 1: info 2: debug
     """
-    def __init__(self, check_freq,  evaluation_set,train_environment,trainset_depth, verbose=1,save_best=False,plot=True,test_on_data_size=True):
+    def __init__(self, check_freq,  evaluation_set,train_environment,trainset_depth, verbose=1,save_best=False,plot=True,test_on_data_size=None):
         super(CustomCallback, self).__init__(verbose)
         self.save_best=save_best
         self.plot=plot
@@ -74,18 +74,22 @@ class CustomCallback(BaseCallback):
         self.results_path=os.getcwd()+'/src/rlnoise/bench_results/'
         self.environment=train_environment
         self.best_mean_reward = -np.inf
-        self.train_circ=evaluation_set['train_set']
-        
-        self.train_label=evaluation_set['train_label']
+        if self.test_on_data_size is not None:
+            self.train_circ=evaluation_set['train_set'][:test_on_data_size]
+            self.train_label=evaluation_set['train_label'][:test_on_data_size]
+        else:
+            self.train_circ=evaluation_set['train_set']
+            self.train_label=evaluation_set['train_label']
+
         self.val_circ=evaluation_set['val_set']
         self.val_label=evaluation_set['val_label']
-        self.dataset_size=len(evaluation_set)
+        self.dataset_size=len(self.train_circ)
         self.trainset_depth=trainset_depth
         self.n_qubits=self.train_circ[0].shape[1]
         self.eval_results=[]
         self.train_results=[]
         self.timestep_list=[]
-        self.save_path = os.path.join(self.log_dir, 'best_model_Q%d_D%d'%(self.n_qubits,self.trainset_depth))
+        self.save_path = os.path.join(self.log_dir, 'best_model_set1000_Box_high0.2_Q%d_D%d'%(self.n_qubits,self.trainset_depth))
         # Those variables will be accessible in the callback
         # (they are defined in the base class)
         # The RL model
@@ -159,37 +163,51 @@ class CustomCallback(BaseCallback):
         self.timestep_list=np.array(self.timestep_list)
         if self.plot is True:
             self.plot_results()
-        if self.test_on_data_size is True:
-            f = open(self.results_path+"test_size_D_%d_Dep-Term_CZ_3Q_%d.npz"%(self.trainset_depth,self.dataset_size),"wb")
+        '''
+        if self.test_on_data_size is not None:
+            f = open(self.results_path+"test_size%d_D_%d_Dep-Term_CZ_3Q.npz"%(self.dataset_size,self.trainset_depth),"wb")
             np.savez(f,train_results=self.train_results, val_results=self.eval_results, timesteps=self.timestep_list)
             f.close()
+        '''
         pass 
 
     def plot_results(self):
         train_results=np.array(self.train_results)
         eval_results=np.array(self.eval_results)
         time_steps=np.array(self.timestep_list)/1000
-        
-        fig=plt.figure(figsize=(15,5))
-        fig.suptitle('3Q w CZ D7 K3 SR-on (+- 0.1) Dep and Therm', fontsize=15)
-        ax=fig.add_subplot(131)
-        ax1=fig.add_subplot(132)
-        ax2=fig.add_subplot(133)
-        plt.subplots_adjust(left=0.065, bottom=None, right=0.971, top=None, wspace=0.27, hspace=None)
-        ax.plot(time_steps,eval_results[:,0],label='evaluation_set',marker='x')
-        ax.set(xlabel='timesteps/1000', ylabel='Reward',title='Average final reward')
-        ax1.plot(time_steps,eval_results[:,1],marker='x')
-        ax1.set(xlabel='timesteps/1000', ylabel='H-S distance',title='Hilbert-Schmidt distance between DM')
-        ax2.plot(time_steps,eval_results[:,2],marker='x')
-        ax2.set(xlabel='timesteps/1000', ylabel='Trace Distance',title='Trace distance between DM')
 
-        ax.plot(time_steps,train_results[:,0],color='orange',label='train_set',marker='x')
-        ax1.plot(time_steps,train_results[:,1],color='orange',label='train_set',marker='x')
-        ax2.plot(time_steps,train_results[:,2],color='orange',label='train_set',marker='x')
-        ax.legend()
-        
-        fig.savefig(self.plot_dir+'Q%d_D%d_SR_steps%d'%(self.n_qubits,self.trainset_depth,self.timestep_list[-1]))
-        plt.show()
+        if self.test_on_data_size is None:
+            fig=plt.figure(figsize=(15,5))
+            fig.suptitle('3Q w CZ D7 K3 dataset len=1000 Dep and Therm', fontsize=15)
+            ax=fig.add_subplot(131)
+            ax1=fig.add_subplot(132)
+            ax2=fig.add_subplot(133)
+            plt.subplots_adjust(left=0.065, bottom=None, right=0.971, top=None, wspace=0.27, hspace=None)
+            ax.plot(time_steps,eval_results[:,0],label='evaluation_set',marker='x')
+            ax.set(xlabel='timesteps/1000', ylabel='Reward',title='Average final reward')
+            ax1.plot(time_steps,eval_results[:,1],marker='x')
+            ax1.set(xlabel='timesteps/1000', ylabel='H-S distance',title='Hilbert-Schmidt distance between DM')
+            ax2.plot(time_steps,eval_results[:,2],marker='x')
+            ax2.set(xlabel='timesteps/1000', ylabel='Trace Distance',title='Trace distance between DM')
+
+            ax.plot(time_steps,train_results[:,0],color='orange',label='train_set',marker='x')
+            ax1.plot(time_steps,train_results[:,1],color='orange',label='train_set',marker='x')
+            ax2.plot(time_steps,train_results[:,2],color='orange',label='train_set',marker='x')
+            ax.legend()
+            
+            fig.savefig(self.plot_dir+'set1000_Q%d_D%d_SR_steps%d'%(self.n_qubits,self.trainset_depth,self.timestep_list[-1]))
+            plt.show()
+        else:
+            fig=plt.figure(figsize=(5,5))
+            fig.suptitle('3Q w CZ D7 K3 SR-off train dataset size=%d, val dataset size=%d'%(self.dataset_size,len(self.val_circ)) , fontsize=15)
+            ax=fig.add_subplot(111)
+            ax.set(xlabel='timesteps/1000', ylabel='Reward',title='Average final reward')
+            ax.plot(time_steps,eval_results[:,0],label='evaluation_set',marker='x')
+            ax.plot(time_steps,train_results[:,0],color='orange',label='train_set',marker='x')
+            ax.legend()
+            fig.savefig(self.plot_dir+'test_size%d_D_%d_Dep-Term_CZ_%dQ'%(self.dataset_size,self.trainset_depth,self.n_qubits))
+            #plt.show()
+
 
     def generalization_test():
         #here will be tested the simple generalization (1 depth for train and different for test)
