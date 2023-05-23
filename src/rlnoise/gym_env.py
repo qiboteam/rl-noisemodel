tested discrete action (not good results) and added a corrected formula of Fidelity, also added Bures distance metric

src/rlnoise/gym_env.py
@@ -7,6 +7,8 @@ from qibo import gates
 from qibo.quantum_info import trace_distance
 from rlnoise.dataset import gate_to_idx
 
+
+
 def gate_action_index(gate):
     if gate == 'epsilon_x':
         return 0
@@ -64,17 +66,13 @@ class QuantumCircuit(gym.Env):
 
         elif self.action_space_type=="Discrete":
             if noise_param_space is None:
-                self.noise_par_space = { 'range': (0,0.1), 'n_steps': 100 }
+                self.noise_par_space = { 'max': 1., 'n_steps': 20}
             else:
                 self.noise_par_space = noise_param_space
-            self.noise_incr = np.diff(self.noise_par_space['range']) / self.noise_par_space['n_steps']
-            assert self.noise_incr > 0
-
-        elif self.action_space_type=="Binary":
-            action_shape=[2 for _ in range(self.n_qubits*4)]
-            self.action_space = spaces.MultiDiscrete( 
-            action_shape
-            )
+            self.discrete_step = self.noise_par_space['max'] / self.noise_par_space['n_steps']
+            assert self.discrete_step > 0
+            action_shape=[self.noise_par_space['n_steps'] for _ in range(self.n_qubits*4)]
+            self.action_space=spaces.MultiDiscrete(action_shape)
         
     def init_state(self, i=None):
         if i is None:
@@ -107,12 +105,18 @@ class QuantumCircuit(gym.Env):
         self.position=0
         self.current_state, self.current_target = self.init_state(i)
         return self._get_obs()
-
-    def step(self, action):
+    
+    def transform_action(self, action):
+        """
+        Trasform discrete action in the form of a continuos action with"""
         
+        action2=action.reshape((self.n_qubits,4))*self.discrete_step
+        
+        return action2
+    def step(self, action):
+        if self.action_space_type=="Discrete":
+            action=self.transform_action(action)
         self.action=action
-        if self.action_space_type =="Binary" or self.action_space_type =="Discrete":
-            action=action.reshape((self.n_qubits,4))
         reward=0.
         position = self.get_position()
         if self.step_reward is True:
@@ -124,7 +128,7 @@ class QuantumCircuit(gym.Env):
                 raise("Error: problem with the specified Step_Reward_Metric")
        
         for q in range(self.n_qubits):          
-            for idx, a in enumerate(action[q]):
+            for idx, a in enumerate(self.action[q]):
                 #print('considering qubit: ',q,'and action: ',action, 'at position: ',position)
                 if a!=0:
                     reward -= self.action_penality

src/rlnoise/utils.py
@@ -5,7 +5,8 @@ from configparser import ConfigParser
 from rlnoise.gym_env import QuantumCircuit
 import copy
 from rlnoise.custom_noise import CustomNoiseModel
-from qibo.quantum_info import trace_distance, fidelity #those are the equivalent of fidellity for density matrices (see also Bures distance)
+from qibo.quantum_info import trace_distance #(see also Bures distance)
+from scipy.linalg import sqrtm
 np.set_printoptions(precision=3, suppress=True)
 
 DEBUG=False
@@ -102,6 +103,31 @@ def plot_results(train_history, val_history, n_steps=20, filename="train_info.pn
     plt.show()
     plt.savefig(figures_folder()+ '/' +filename)
 
+def compute_fidelity(density_matrix0, density_matrix1):
+    """Compute the fidelity for two density matrices (pure or mixed states).
+
+    .. math::
+            F( \rho , \sigma ) = -\text{Tr}( \sqrt{\sqrt{\rho} \sigma \sqrt{\rho}})^2
+    """
+    sqrt_mat = sqrtm(density_matrix0)
+    sqrt_mat_sqrt = sqrt_mat @ density_matrix1 @ sqrt_mat
+
+    evs = np.linalg.eigvalsh(sqrt_mat_sqrt)
+    evs = np.real(evs)
+    evs = np.where(evs > 0.0, evs, 0.0)
+    trace = (np.sum(np.sqrt(evs))) ** 2
+
+    return trace
+
+def bures_distance(density_matrix0, density_matrix1):
+    """ Compute the Bures distance between density matrices
+    .. math::
+        B( \rho , \sigma ) = -\sqrt{2*(1-sqrt(F(\sigma,\rho)))} where F is the fidelity
+    """
+    return np.sqrt(2*(1-np.sqrt(compute_fidelity(density_matrix0, density_matrix1))))
+
+
+
 def model_evaluation(evaluation_circ,evaluation_labels,train_environment,model):
     '''
     Function for evaluating the model
@@ -156,10 +182,10 @@ def model_evaluation(evaluation_circ,evaluation_labels,train_environment,model):
         predicted_rep=environment.get_circuit_rep()
         dm_untrained=np.array(predicted_circ().state())
         avg_rew.append(rewards)
-        avg_fidelity.append(fidelity(evaluation_labels[i],dm_untrained))
+        avg_fidelity.append(bures_distance(evaluation_labels[i],dm_untrained))
         avg_trace_distance.append(trace_distance(evaluation_labels[i],dm_untrained))
         if i==0 and DEBUG:
-            noise_model=CustomNoiseModel() #NB must be initialized with args from config
+            noise_model=CustomNoiseModel(primitive_gates=params.get('noise','primitive_gates'),lam=params.get('noise','dep_lambda'),p0=params.get('noise','p0'),x_coherent_on_gate=['rx'],z_coherent_on_gate=['rz'],epsilon_x=params.get('noise','epsilon_x'),epsilon_z=params.get('noise','epsilon_z'),damping_on_gate=params.get('noise','damping_on_gate'),depol_on_gate=params.get('noise','depol_on_gate')) 
             test_rep=evaluation_circ[i]
             test_circ=noise_model.apply(train_environment.rep.rep_to_circuit(test_rep))
             print('\nTrue noisy circuit')
@@ -186,3 +212,5 @@ def test_representation():
     print(array)
     print(' --> Circuit Rebuilt:\n', rep.array_to_circuit(array).draw())
 '''
+
+