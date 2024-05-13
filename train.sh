cde17a04a2919ccf81b70bca180eee5df3508aa8
simone bordoni <simone.bordoni@uniroma1.it>
2024-05-13 13:10:00 +0400

    mixed dataset upload

--- train.sh
+++ train.sh
@@ -2,6 +2,6 @@
 #SBATCH --job-name=rl-train
 #SBATCH --output=train.out
 #SBATCH --mail-user=simone.bordoni@uniroma1.it
-#SBATCH --mail-type=END  # Send email when job ends
+#SBATCH --mail-type=ALL
 
 python ./simulation/model_train.py
