20f8db8e7edf0ec023d4c4d52a23fa0dc9631c79
simone bordoni <simone.bordoni@uniroma1.it>
2024-05-21 10:02:15 +0400

    new models

--- plots/Train.py
+++ plots/Train.py
@@ -3,9 +3,9 @@ import numpy as np
 import scienceplots
 plt.style.use('science')
 
-exp_folder = "simulation/experiments/3q_squared/"
+exp_folder = "simulation/experiments/3q_mixed_dataset_big/"
 results_path = exp_folder + "model_train_result.npz"
-steps = 120
+steps = 500
 
 SMALL_SIZE = 22
 MEDIUM_SIZE = 26
