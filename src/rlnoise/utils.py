953db79451bc253b1870cd077375e1f736ed4ae6
simone bordoni <simone.bordoni@uniroma1.it>
2024-08-01 19:36:30 +0400

    estimate error

--- src/rlnoise/utils.py
+++ src/rlnoise/utils.py
@@ -1,10 +1,10 @@
 import numpy as np
-from qibo import gates
+from qibo import gates, set_backend
 from qibo.models.circuit import Circuit
-from qibo.transpiler.unroller import Unroller, NativeGates
-from qibo.transpiler.optimizer import Rearrange
-from qibo.quantum_info import trace_distance as qibo_trace_distance, fidelity
-from scipy.linalg import sqrtm
+from qibo.quantum_info import trace_distance as qibo_trace_distance
+from scipy.linalg import sqrtm, svd
+from qibo.gates import ReadoutErrorChannel
+from qibo.backends import  GlobalBackend
 
 def analyze_circuit(circuit: Circuit, model, noise_model=None):
     """Analyze the gates added by the given model to a circuit.
@@ -234,6 +234,69 @@ def qft():
     
     return circuit
 
-def estimate_hardware_noise(dms_rl, dms_true):
-    """Estimate the hardware noise due to measurements and shot noise from the density matrices obtained with the model."""
-    return NotImplementedError
+
+def trace_distance(rho, sigma):
+    diff = rho - sigma
+    s = svd(diff, compute_uv=False)
+    return 0.5 * np.sum(np.abs(s))
+
+
+def numerical_partial_derivative(rho, sigma, epsilon, i, j):
+    perturbed_rho = np.copy(rho)
+    perturbed_rho[i, j] += epsilon
+    return (trace_distance(perturbed_rho, sigma) - trace_distance(rho, sigma)) / epsilon
+
+
+def get_delta_rho(rho, lam, nshots, readout_f):
+    set_backend("numpy")
+    X = np.array([[0, 1], [1, 0]])
+    Y = np.array([[0, -1j], [1j, 0]])
+    Z = np.array([[1, 0], [0, -1]])
+    I = np.eye(2)
+
+    backend = GlobalBackend()
+    readout = ReadoutErrorChannel(
+        0, [[readout_f, 1-readout_f], [1-readout_f, readout_f]])
+    x_to_z = gates.H(0).matrix(backend=backend)
+    y_to_z = gates.S(0).matrix(
+        backend=backend)@gates.H(0).matrix(backend=backend)
+    rho_x = readout.apply_density_matrix(
+        state=x_to_z@rho@x_to_z, nqubits=1, backend=backend)
+    rho_y = readout.apply_density_matrix(
+        state=y_to_z.conjugate().transpose()@rho@y_to_z, nqubits=1, backend=backend)
+    rho_z = readout.apply_density_matrix(state=rho, nqubits=1, backend=backend)
+
+    a_x = np.trace(rho@X)/2
+    a_y = np.trace(rho@Y)/2
+    a_z = np.trace(rho@Z)/2
+    delta_ax = np.sqrt(1-a_x**2)/np.sqrt(nshots) + lam * \
+        np.abs(a_x) + np.abs(np.trace(rho_x@Z)/2-a_x)
+    delta_ay = np.sqrt(1-a_y**2)/np.sqrt(nshots) + lam * \
+        np.abs(a_y) + np.abs(np.trace(rho_y@Z)/2-a_y)
+    delta_az = np.sqrt(1-a_z**2)/np.sqrt(nshots) + \
+        np.abs(np.trace(rho_z@Z)/2-a_z)
+    delta_rho = delta_ax*X + delta_ay*Y + delta_az*Z
+    return delta_rho
+
+def estimate_hardware_noise(dms_rl, dms_true, lam = 0.004, readout_f = 0.96, nshots = 4000):
+    """Estimate the hardware noise due to measurements and shot noise from the density matrices obtained with the model.
+    Args:
+        dms_rl (np.array): density matrices obtained with the model
+        dms_true (np.array): density matrices btained with state tomography
+        lam (float): depolarizing parameter
+        readout_f (float): readout fidelity
+        nshots (int): number of shots
+    """
+    epsilon = 1e-6
+
+    delta_dms_true = get_delta_rho(dms_true, lam, nshots, readout_f)
+
+    D = trace_distance(dms_true, dms_rl)
+
+    partial_derivatives_dms_true = np.zeros_like(dms_true)
+    for i in range(dms_true.shape[0]):
+        for j in range(dms_true.shape[1]):
+            partial_derivatives_dms_true[i, j] = numerical_partial_derivative(
+                dms_true, dms_rl, epsilon, i, j)
+            
+    delta_D = np.sum(np.abs(partial_derivatives_dms_true * delta_dms_true))
